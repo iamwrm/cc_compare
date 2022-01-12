f462af2679ef125bfa25c7735399a3c3c3c7c509
Ren Wang <iamwrm@gmail.com>
2022-01-12 15:24:03 +0000

    fix bench

--- src/bench.cpp
+++ src/bench.cpp
@@ -4,45 +4,38 @@
 
 static void BM_with(benchmark::State& state)
 {
+    double x { 0.1 };
     for (auto _ : state) {
-        benchmark::DoNotOptimize(with_attributes::cos(0.1));
-        benchmark::DoNotOptimize(with_attributes::cos(0.1));
+        benchmark::DoNotOptimize(with_attributes::cos(x));
     }
 }
 BENCHMARK(BM_with);
 
 static void BM_without(benchmark::State& state)
 {
+    double x { 0.1 };
     for (auto _ : state) {
-        benchmark::DoNotOptimize(no_attributes::cos(0.1));
+        benchmark::DoNotOptimize(no_attributes::cos(x));
     }
 }
 BENCHMARK(BM_without);
 
 static void BM_std(benchmark::State& state)
 {
+    double x { 0.1 };
     for (auto _ : state) {
-        benchmark::DoNotOptimize(std::cos(0.1));
+        benchmark::DoNotOptimize(std::cos(x));
     }
 }
 BENCHMARK(BM_std);
 
 static void BM_rstd1(benchmark::State& state)
 {
+    double x = gen_random();
     for (auto _ : state) {
-        benchmark::DoNotOptimize(gen_random());
-        benchmark::DoNotOptimize(gen_random());
+        benchmark::DoNotOptimize(std::cos(x));
     }
 }
 BENCHMARK(BM_rstd1);
-
-static void BM_rstd2(benchmark::State& state)
-{
-    for (auto _ : state) {
-        auto input = gen_random();
-        benchmark::DoNotOptimize(std::cos(input));
-    }
-}
-BENCHMARK(BM_rstd2);
 
 BENCHMARK_MAIN();
